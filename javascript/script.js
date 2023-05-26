e3824af7a4ac6ebfc27d90031e3bc8af4e5cf33d
BlueDoraemon <3803480+BlueDoraemon@users.noreply.github.com>
2023-05-26 13:04:42 +1000

    Added function addbook removeBook createBook test functions

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,39 @@
-let library = {};
+let library = []; // array of books
 
-function book(name, author, pages, read){
+function Book(name, author, pages, read){
     this.name = name;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.hasRead() = () => {
+        (read) ? alert(`You've read it`) : alert(`You haven't read it`);
+    };
+
+};
+
+function addBookToLibrary(book) {
+    library.push(this);
+    console.log(library);
 }
+
+function removeBook(int) {
+    library.splice(int,1);
+}
+
+
+function createBook(name, author, pages, read){
+    const book = new Book(name, author, pages, read);
+}
+const book1 = new Book(`The Guest List: A Reese's Book Club Pick`,`Lucy Foley`, 336,false);
+const book2 = new Book(`The Guest List: A Reese's Book Club Pick`,`Lucy Foley`, 336,false);
+const book3 = new Book(`The Guest List: A Reese's Book Club Pick`,`Lucy Foley`, 336,false);
+//make a function that creates a book
+
+
+
+//testing
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+console.log(library);
